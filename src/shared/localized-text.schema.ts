3e2ca81c9b28cd4eb8407e92f94cc18233517160
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-09-11 17:40:15 +0400

    Add localized text schema with English and Arabic support

---
+++ src/shared/localized-text.schema.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+export const localizedTextSchema = new Schema(
+  {
+    en: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    ar: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+  },
+  { _id: false },
+);
