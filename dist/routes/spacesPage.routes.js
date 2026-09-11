0fbad5afc73d1f801db3a4c557a789b16f7e2402
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-09-11 17:38:24 +0400

    Add CRUD operations for spaces page routes

---
+++ dist/routes/spacesPage.routes.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { createSpacesPage, getSpacesPage, updateSpacesPage, deleteSpacesPage, } from "../controllers/spacesPage.controller.js";
+const router = Router();
+router.post("/", createSpacesPage);
+router.get("/", getSpacesPage);
+router.patch("/", updateSpacesPage);
+router.delete("/", deleteSpacesPage);
+export default router;
