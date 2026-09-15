0a81488048c811679db264d005906e168a7d97ac
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-09-15 09:43:26 +0400

    Add User model with schema for user details, roles, and authentication

---
+++ src/models/user.model.ts
@@ -0,0 +1,50 @@
+import mongoose, { Schema } from "mongoose";
+
+export enum UserRole {
+  ADMIN = "admin",
+  USER = "user",
+}
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+
+    role: {
+      type: String,
+      enum: Object.values(UserRole),
+      default: UserRole.USER,
+    },
+
+    refreshToken: {
+      type: String,
+      default: null,
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
