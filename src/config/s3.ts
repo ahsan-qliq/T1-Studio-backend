ee7e8f0c36efeee0867ec224ee449f02a72a1bf4
ahsan-qliq <uiuxdev1@ezdigital.ae>
2026-09-18 09:22:04 +0400

    feat: add S3 credentials configuration to s3Client initialization

--- src/config/s3.ts
+++ src/config/s3.ts
@@ -2,4 +2,10 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 export const s3Client = new S3Client({
   region: process.env.AWS_REGION,
+  ...(process.env.AWS_ACCESS_KEY_ID && {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    },
+  }),
 });
